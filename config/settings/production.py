506667c3a62508a31deb692ffad03ffe5b0defc4
Mijhael Samaniego <mijhaelsamaniego@konecta.com.py>
2023-08-28 11:03:16 -0400

    Se actualiuza la región de mails

--- config/settings/production.py
+++ config/settings/production.py
@@ -114,7 +114,11 @@ INSTALLED_APPS += ["anymail"]  # noqa: F405
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
 # https://anymail.readthedocs.io/en/stable/esps/amazon_ses/
 EMAIL_BACKEND = "anymail.backends.amazon_ses.EmailBackend"
-ANYMAIL = {}
+ANYMAIL = {
+    "AMAZON_SES_CLIENT_PARAMS": {
+        "region_name": "sa-east-1",
+    },
+}
 
 # Collectfast
 # ------------------------------------------------------------------------------
