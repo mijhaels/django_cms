dc191491d28172e64f3c73916ae23d639cd0d1b0
luceroem <luceestigarribia@fpuna.edu.py>
2023-09-07 16:44:45 -0400

    Corrección del nombre del módulo de contenido

--- django_cms/contenido/apps.py
+++ django_cms/contenido/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ContenidoConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "contenido"
+    name = "django_cms.contenido"
