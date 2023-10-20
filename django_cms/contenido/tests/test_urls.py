d25f66d32c334b32c84db331b233e96160ef79af
Dahiana <davalosdahiana@fpuna.edu.py>
2023-10-19 23:52:35 -0300

    Actualización de test de contenido

--- django_cms/contenido/tests/test_urls.py
+++ django_cms/contenido/tests/test_urls.py
@@ -3,6 +3,7 @@ from django.urls import resolve, reverse
 
 from django_cms.contenido.tests.factories import CategoriaFactory, ContenidoFactory
 from django_cms.contenido.views import ContenidoDetalleView
+from django_cms.contenido.views import ContenidoBusquedaView
 
 
 class ContenidoURLsTest(TestCase):
@@ -15,10 +16,14 @@ class ContenidoURLsTest(TestCase):
         url = reverse("contenido:contenido_detalle", args=[self.contenido.id])
         self.assertEqual(resolve(url).func.view_class, ContenidoDetalleView)
 
+    def test_contenido_busqueda_url(self):
+        url = reverse("contenido:contenido_busqueda")
+        self.assertEqual(resolve(url).func.view_class, ContenidoBusquedaView)
+
 
 """ class CategoriaURLsTest(TestCase):
     def setUp(self):
         self.categoria = CategoriaFactory()
 
     # Add your Categoria related URL tests here
- """
+"""
--- django_cms/contenido/tests/test_views.py
+++ django_cms/contenido/tests/test_views.py
@@ -3,7 +3,6 @@ from django.urls import reverse
 
 from .factories import ContenidoFactory
 
-
 class ContenidoDetalleViewTest(TestCase):
     def setUp(self):
         self.client = Client()
@@ -15,3 +14,14 @@ class ContenidoDetalleViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "pages/contenido_detalle.html")
         self.assertEqual(response.context["contenido"], self.contenido)
+
+class ContenidoBusquedaViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.contenido = ContenidoFactory.create()
+        self.url = reverse("contenido:contenido_busqueda")
+
+    def test_contenido_busqueda_view_with_params(self):
+        response = self.client.get(self.url, {"publicacion": self.contenido.titulo})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "pages/busqueda_resultados.html")
